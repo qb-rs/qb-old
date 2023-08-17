d7386f207d72d9a44f56065bdeccfaa30543fdb1
Lucas Birkert (Laptop) <kekontheworld@gmail.com>
2023-08-17 21:34:42 +0200

    Setup migration for user table
    This table models the base user and its properties such as a display
    name, an internal id, a human-readable id and its password. (argon2)

--- crates/qb-migration/src/m20220101_000001_create_table.rs
+++ crates/qb-migration/src/m20220101_000001_create_table.rs
@@ -5,7 +5,7 @@
 // █─██▀─██─██─███─███▀─▀███─▄─▀██▄─▄████─████─▄█▀█
 // ▀───▄▄▀▀▄▄▄▄▀▀▄▄▄▀▄▄█▄▄▀▄▄▄▄▀▀▀▄▄▄▀▀▀▄▄▄▀▀▄▄▄▄▄▀
 // https://github.com/QuixByte/qb/blob/main/LICENSE
-// 
+//
 // (c) Copyright 2023 The QuixByte Authors
 
 use sea_orm_migration::prelude::*;
@@ -16,42 +16,47 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(User::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Post::Id)
+                        ColumnDef::new(User::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(User::Name).string().not_null())
+                    .col(ColumnDef::new(User::DisplayName).string().not_null())
+                    .col(ColumnDef::new(User::Password).string().not_null())
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(User::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum User {
     Table,
+    // Used internally to identify users. An id is immutable and will **NEVER** be updated. The
+    // only time that an id is invalidated is after a successful account deletion.
     Id,
-    Title,
-    Text,
+    // Used to for QB's users as a readable identifier. This can be updated via the web interface
+    // but is strongly ratelimited. A name identifier must only contain the letters in the latin
+    // alphabeth and its length must be contained in the range 4..=14 => /^[a-zA-Z_]{4, 14}$/
+    Name,
+    // Used as a more customizable way for users to update their name. This can include any special
+    // characters as long the length is contained in the range 1..=50 => /^.{1, 50}$/
+    DisplayName,
+    // Contains the digest of the argon2 hashing algorithm applied to a randomly generated salt
+    // and the password the user provided when registering/resetting their password.
+    Password,
 }
--- crates/qb-migration/src/main.rs
+++ crates/qb-migration/src/main.rs
@@ -12,5 +12,5 @@ use sea_orm_migration::prelude::*;
 
 #[async_std::main]
 async fn main() {
-    cli::run_cli(migration::Migrator).await;
+    cli::run_cli(qb_migration::Migrator).await;
 }
